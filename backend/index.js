5950146e45c6bb03043d7cdf1932c71f8dbd6b5b
shivansh kamboj <kambojshivansh2004@gmail.com>
2025-05-01 14:16:03 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -33,10 +33,6 @@ app.use("/api/messages", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
 }
 
 // ⚡ Initialize socket with server
